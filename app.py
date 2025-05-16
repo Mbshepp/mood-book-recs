75337ccd325768efa492e5260bfd279c0791eb3d
Mitsu Shepperd <mitsu.shepperd@gmail.com>
2025-05-15 21:38:51 -0400

    Created the Mood/Synonyms Dictionary of Lists

---
+++ app.py
@@ -0,0 +1,17 @@
+moods_synonyms = {
+    'Happy': ['happy','cheerful','elated'],
+    'Sad': ['sad','melancholy','downcast'],
+    'Enchanted': ['enchanted','captivated','wonderment'],
+    'Inspired': ['inspired','stimulated','emboldened'],
+    'Motivated': ['motivated','driven','hopeful'],
+    'Nostalgic': ['nostalgic','sentimental','wishful'],
+    'Curious': ['curious','inquisitive','questioning'],
+    'Grateful': ['grateful','thankful', 'appreciative'],
+    'Lonely': ['lonely','isolated','forlorn'],
+    'Playful': ['playful','whimsical','goofy'],
+    'Mad': ['mad','furious','irate'],
+    'Bored': ['bored','uninterested','disinterested'],
+    'Anxious': ['anxious','stressed','troubled'],
+    'Humorous': ['humorous','amusing','witty'],
+    'Serious': ['serious','solemn','earnest']
+}
