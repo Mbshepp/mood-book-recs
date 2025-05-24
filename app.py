3b8495324593e7531f8d7950550a66f851c093d1
Mitsu Shepperd <mitsu.shepperd@gmail.com>
2025-05-23 21:42:07 -0400

    Added a wait for selector, set mood variable to .lower(), added a click for the User_Mood variable, and a page pause

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from flask import Flask
+from PIL import Image
+from playwright.sync_api import sync_playwright
+
+'''
 moods_synonyms = {
     'Happy': ['happy','cheerful','elated'],
     'Sad': ['sad','melancholy','downcast'],
@@ -15,3 +20,23 @@ moods_synonyms = {
     'Humorous': ['humorous','amusing','witty'],
     'Serious': ['serious','solemn','earnest']
 }
+'''
+
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://booksbymood.com/")
+    page.wait_for_selector("h2.text-3xl.font-semibold.text-accent.text-center.drop-shadow-md")
+    timeout=5000
+    User_Mood = "Happy".lower()
+    page.click(f"text={User_Mood}")
+    page.pause()
+    browser.close()
+
+    
+
+
+
+
+
