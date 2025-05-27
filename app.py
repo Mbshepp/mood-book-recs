13429f0d3b8f00484ea3f2a0cb9d4b4c591d4619
Mitsu Shepperd <mitsu.shepperd@gmail.com>
2025-05-27 15:35:34 -0400

    [200~⦁Removed the last two indices of the sorted_high_low to keep the top three highest ratings. Changed the list of dictionaries into a list of lists and updated all code to reflect this change. I have commented out all of the code for the list of dictionaries in case I need to revert back later.

--- app.py
+++ app.py
@@ -37,11 +37,24 @@ moods_synonyms = {
 
 titles_and_ratings_list = []
 recommended_books_list = []
-title_rating_dict ={}
+#title_rating_dict ={}
+title_rating_list = []
 book_title = None
 
+
+def open_webpage_choose_mood():
+    global page
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://booksbymood.com/")
+    page.wait_for_selector("h2.text-3xl.font-semibold.text-accent.text-center.drop-shadow-md")
+    timeout = 5000
+    page.click(f"text={User_Mood}")
+
+
 def scrape_book_info():
-    global title_rating_dict, book_title
+    #global title_rating_dict, book_title
+    global title_rating_list, book_title
     while len(titles_and_ratings_list) < 5:
         book_title = page.inner_text("h2")
         if book_title in recommended_books_list:
@@ -52,34 +65,59 @@ def scrape_book_info():
             book_rating = (page.inner_text(rating_selector))
             a, b = book_rating.split("/")
             float_book_rating = float(a) / float(b)
-            title_rating_dict = {book_title: float_book_rating}
-            titles_and_ratings_list.append(title_rating_dict)
+            #title_rating_dict = {book_title: float_book_rating}
+            title_rating_list= [book_title, float_book_rating]
+            print("test: title_rating_list")
+            print(title_rating_list)
+            #titles_and_ratings_list.append(title_rating_dict)
+            titles_and_ratings_list.append(title_rating_list)
+            print("test: titles and ratings list")
+            print(titles_and_ratings_list)
             page.get_by_text("Next Book").scroll_into_view_if_needed()
             page.click(f"text={'Next Book'}")
-    #titles_and_ratings_list.clear()
 
-    return title_rating_dict
-
+    #return title_rating_dict
+    return title_rating_list
 
 def three_highest_ratings():
-    sorted_high_low = sorted(titles_and_ratings_list, key=lambda x: list(x.values())[0], reverse=True)
-    print('SORTED HIGH TO LOW')
+    global top_three_rated
+    top_three_rated = []
+    #sorted_high_low = sorted(titles_and_ratings_list, key=lambda x: list(x.values())[0], reverse=True)
+    sorted_high_low = sorted(titles_and_ratings_list, key=lambda x: x[1], reverse=True)
+    print("test: sorted_high_low")
     print(sorted_high_low)
+    top_three_rated = sorted_high_low[:3]
+    print("test: top three rated")
+    print(top_three_rated)
 
+
+    return top_three_rated
+
+
+def scrape_top_three_books_full_info():
+    for book in top_three_rated:
+        open_webpage_choose_mood()
+        author_selector = 'span.text-gray-500 drop-shadow-md\\"["Gretchen Rubin"]'
+        author = (page.inner_text(author_selector))
+        print("AUTHOR SELECTOR AND AUTHOR")
+        print(author_selector)
+        print(author)
+
+'''book_title_and_key_not_equal = book_title != list(titles_and_ratings_list)[0]
+       if book_title_and_key_not_equal:
+            page.get_by_text("Next Book").scroll_into_view_if_needed()
+            page.click(f"text={'Next Book'}")
+       else:'''
 
 
 
 User_Mood = "Happy".lower()
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://booksbymood.com/")
-    page.wait_for_selector("h2.text-3xl.font-semibold.text-accent.text-center.drop-shadow-md")
-    timeout=5000
-    page.click(f"text={User_Mood}")
+    open_webpage_choose_mood()
     scrape_book_info()
     three_highest_ratings()
+    scrape_top_three_books_full_info()
     #page.pause()
 
 
